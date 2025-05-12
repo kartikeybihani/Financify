0214dbe71a0cda67656a84c9d20dbe0c565925f7
Kartik Bihani <kartikeybihani05@gmail.com>
2025-05-11 17:23:37 -0700

    fix: update plaid client configuration

--- app/plaidClient.js
+++ app/plaidClient.js
@@ -2,7 +2,7 @@ import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
 
 const config = new Configuration({
   basePath: PlaidEnvironments.sandbox,
-  basePath: PlaidEnvironments.production,
+  // basePath: PlaidEnvironments.production,
   baseOptions: {
     headers: {
       "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
