a2bc8c247dadef8ba19ce6446963ca0acddda28a
Kartik Bihani <kartikeybihani05@gmail.com>
2025-06-09 22:14:30 +0530

    production keys fix

--- app/plaidClient.js
+++ app/plaidClient.js
@@ -2,6 +2,7 @@ import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
 
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_SECRET_PROD = process.env.PLAID_SECRET_PROD;
 const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
 
 const config = new Configuration({
@@ -12,7 +13,8 @@ const config = new Configuration({
   baseOptions: {
     headers: {
       "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
-      "PLAID-SECRET": PLAID_SECRET,
+      "PLAID-SECRET":
+        PLAID_ENV === "production" ? PLAID_SECRET_PROD : PLAID_SECRET,
     },
   },
 });
