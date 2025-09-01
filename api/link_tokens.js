5b767d77d66f4525268ed28f42465494b396b948
Kartik Bihani <kartikeybihani05@gmail.com>
2025-09-01 02:08:18 -0700

    fix 2

--- api/link_tokens.js
+++ api/link_tokens.js
@@ -57,8 +57,8 @@ export default async function handler(req, res) {
       const { data: tokenData } = await client.linkTokenCreate({
         user: { client_user_id: user_id },
         client_name: "Financify",
-        products: ["auth"], // minimum default
-        required_if_supported_products: ["liabilities"],
+        products: ["auth", "liabilities"], // minimum default
+        required_if_supported_products: [],
         optional_products: ["investments", "transactions"],
         additional_consented_products: [],
         country_codes: ["US"],
